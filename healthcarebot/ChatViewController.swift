b3a085512b10a6c70b472caf9ed1c5d44705b197
Mathias Klenk <mathiasklenk@gmx.de>
2016-09-11 02:13:56 -0700

    added feature to send pictures

--- healthcarebot/ChatViewController.swift
+++ healthcarebot/ChatViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 import JSQMessagesViewController
 import ConversationV1
 
-class ChatViewController: JSQMessagesViewController {
+class ChatViewController: JSQMessagesViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
     
     private var messages = [JSQMessage]()
     private var outgoingBubbleImageView = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleBlueColor())
@@ -22,7 +23,7 @@ class ChatViewController: JSQMessagesViewController {
         
         senderId = "id"
         senderDisplayName = "Me"
-        self.title = "HealthcareBot"
+        self.title = "Doctor Watson"
 
         
         automaticallyScrollsToMostRecentMessage = true
@@ -61,12 +62,83 @@ class ChatViewController: JSQMessagesViewController {
                 self.collectionView?.reloadData()
                 self.scrollToBottomAnimated(true)
             }
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        let photoItem = JSQPhotoMediaItem(image: image)
+        self.addMedia(photoItem)
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func didPressAccessoryButton(sender: UIButton) {
+        self.inputToolbar.contentView!.textView!.resignFirstResponder()
+        
+        let sheet = UIAlertController(title: "Media messages", message: nil, preferredStyle: .ActionSheet)
+        
+        let photoAction = UIAlertAction(title: "Send photo", style: .Default) { (action) in
             
-           
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+                let imagePicker = UIImagePickerController()
+                imagePicker.delegate = self
+                imagePicker.sourceType = UIImagePickerControllerSourceType.Camera;
+                imagePicker.allowsEditing = false
+                self.presentViewController(imagePicker, animated: true, completion: nil)
+            }
         }
         
         
+//        let videoAction = UIAlertAction(title: "Send video", style: .Default) { (action) in
+//            /**
+//             *  Add fake video
+//             */
+//            let videoItem = self.buildVideoItem()
+//            
+//            self.addMedia(videoItem)
+//        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        
+        sheet.addAction(photoAction)
+//        sheet.addAction(videoAction)
+        sheet.addAction(cancelAction)
+        
+        self.presentViewController(sheet, animated: true, completion: nil)
     }
+    
+    func addMedia(media:JSQMediaItem) {
+           
+        let message = JSQMessage(senderId: self.senderId, displayName: self.senderDisplayName, media: media)
+        self.messages.append(message)
+        
+        //Picture message
+        // Do any additional setup after loading the view.
+        let failure = { (error: NSError) in print(error) }
+        
+        conversation.message(workspaceID, text: "picture", context: context, failure: failure) { response in
+            print(response.output.text)
+            self.context = response.context
+            let message = JSQMessage(senderId: "Server", senderDisplayName: "Server", date: NSDate(), text: response.output.text[0])
+            
+            let time = dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), 1 * Int64(NSEC_PER_SEC))
+            dispatch_after(time, dispatch_get_main_queue()) {
+                //put your code which should be executed with a delay here
+                self.messages.append(message)
+                self.collectionView?.reloadData()
+                self.scrollToBottomAnimated(true)
+            }
+        }
+        
+        self.collectionView?.reloadData()
+        self.scrollToBottomAnimated(true)
+        
+        //Optional: play sent sound
+        self.finishSendingMessageAnimated(true)
+    }
+    
+    
+    
     // MARK: - CollectionView functions
     
     override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
@@ -91,10 +163,15 @@ class ChatViewController: JSQMessagesViewController {
         let cell = super.collectionView(collectionView, cellForItemAtIndexPath: indexPath) as! JSQMessagesCollectionViewCell
         
         let message = messages[indexPath.item]
+        
+        guard !message.isMediaMessage else {
+            return cell
+        }
+        
         if message.senderId == senderId {
-            cell.textView!.textColor = UIColor.whiteColor()
+            cell.textView?.textColor = UIColor.whiteColor()
         } else {
-            cell.textView!.textColor = UIColor.blackColor()
+            cell.textView?.textColor = UIColor.blackColor()
         }
         
         let attributes = [NSForegroundColorAttributeName: cell.textView!.textColor!, NSUnderlineStyleAttributeName: 1]
