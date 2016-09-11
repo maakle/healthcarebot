5c074a0b3c0854023fcaa84d20c9124eec446eaf
Mathias Klenk <mathiasklenk@gmx.de>
2016-09-11 08:20:06 -0700

    nice  version safe

--- DoctorsAroundViewController.swift
+++ DoctorsAroundViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class DoctorsAroundViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+
+class DoctorsAroundViewController: UIViewController, CLLocationManagerDelegate {
 
     //Outlets
     @IBOutlet weak var map: MKMapView!
     var locationManager: CLLocationManager!
+    var resultSearchController:UISearchController? = nil
+    var selectedPin:MKPlacemark? = nil
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +34,32 @@ class DoctorsAroundViewController: UIViewController, CLLocationManagerDelegate, 
             locationManager.requestAlwaysAuthorization()
             locationManager.startUpdatingLocation()
             map.showsUserLocation = true
-
+        }
+        
+        let locationSearchTable = storyboard!.instantiateViewControllerWithIdentifier("LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for specific doctors"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        locationSearchTable.mapView = map
+        
+        locationSearchTable.handleMapSearchDelegate = self
+        
+    }
+    
+    func getDirections(){
+        if let selectedPin = selectedPin {
+            let mapItem = MKMapItem(placemark: selectedPin)
+            let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+            mapItem.openInMapsWithLaunchOptions(launchOptions)
         }
     }
     
@@ -42,4 +74,45 @@ class DoctorsAroundViewController: UIViewController, CLLocationManagerDelegate, 
     }
     
 }
+
+extension DoctorsAroundViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        map.removeAnnotations(map.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "(city) (state)"
+        }
+        map.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        map.setRegion(region, animated: true)
+    }
+}
+
+extension DoctorsAroundViewController : MKMapViewDelegate {
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView?{
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        pinView?.pinTintColor = UIColor.orangeColor()
+        pinView?.canShowCallout = true
+        let smallSquare = CGSize(width: 30, height: 30)
+        let button = UIButton(frame: CGRect(origin: CGPointZero, size: smallSquare))
+        button.setBackgroundImage(UIImage(named: "car"), forState: .Normal)
+        button.addTarget(self, action: "getDirections", forControlEvents: .TouchUpInside)
+        pinView?.leftCalloutAccessoryView = button
+        return pinView
+    }
+}
+
 
